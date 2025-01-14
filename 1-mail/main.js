9c3cf91b878024e5c2da4e361f52d3747b448219
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-14 15:35:33 +0100

    step logici

---
+++ 1-mail/main.js
@@ -0,0 +1,17 @@
+// Mail
+// Crea una lista di email di invitati ad una festa.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+// Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+// Si può fare? Certo che si basta ragionare un po’.
+// Nota:
+// Non è necessario provvedere alla validazione delle email
+
+// step
+// creare l Array lista invitati con vari esempi di mail
+// creare un prompt dove facciamo inserire la mail
+// creiamo una variabile di verifica per vedere se la mail è presente nella lista
+// cilco for per controllare se la mail è presente
+// se presente messaggio di presenza
+// altrimenti messaggio di rifiuto
