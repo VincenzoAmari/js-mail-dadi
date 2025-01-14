89526d42143b9f585c5ac909da9179c27009d2be
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-14 18:45:58 +0100

    add var

--- 2-dadi/main.js
+++ 2-dadi/main.js
@@ -6,3 +6,6 @@
 // generiamo un numero casuale per il giocatore e uno per il computer tramite 2 variabili e Math.floor e random
 // mostriamo i punteggi con i log
 // determiniamo il vincitore con un if
+
+let Giocatore = Math.floor(Math.random() * 6) + 1;
+let Computer = Math.floor(Math.random() * 6) + 1;
