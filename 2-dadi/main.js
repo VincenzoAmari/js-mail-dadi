31eec21c513e371c5f8ad0487a750b1ce24fd18c
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-14 18:42:52 +0100

    dadi primo step

--- 2-dadi/main.js
+++ 2-dadi/main.js
@@ -1,3 +1,8 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// step logici
+// generiamo un numero casuale per il giocatore e uno per il computer tramite 2 variabili e Math.floor e random
+// mostriamo i punteggi con i log
+// determiniamo il vincitore con un if
